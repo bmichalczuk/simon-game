eb5d205072667171e5109bac989ac5b8a1b98fa0
Bartosz Michalczuk <barmic90@hotmail.com>
2019-11-15 22:39:06 +0100

    add variables for dom nodes

--- src/js/domNodes.js
+++ src/js/domNodes.js
@@ -1,1 +1,7 @@
 export const counterScreen = document.querySelector("#counter-screen");
+export const startGameBtn = document.querySelector("start-game-btn");
+export const strictModeIndicator = document.querySelector("#strict-mode-indicator");
+export const strictModeBtn = document.querySelector("#strict-mode-btn");
+export const powerStich = document.querySelector("#power-switch");
+export const boardBtns = document.querySelectorAll(".board__btn");
+export const board = document.querySelector(".board");
